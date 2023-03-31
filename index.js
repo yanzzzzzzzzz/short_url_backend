ca0c04c7343bc421fafebafa4e29cf57698cb201
Jack Tsai <jtsai@nexify.com.hk>
2023-03-31 17:57:26 +0800

    save and get method update to mongo db process

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const cors = require("cors");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 const app = express();
+const Url = require("./models/url");
+const { response } = require("express");
 
 app.use(cors());
 app.use(express.json());
 const PORT = config.PORT;
+
+const characters =
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let urls = [
   {
     originUrl: "https://chat.openai.com/chat",
@@ -14,36 +19,52 @@ let urls = [
   },
 ];
 function generateRandomString() {
-  const characters =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let result = "";
   for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
 }
-app.post("/shorten", (req, res) => {
+app.post("/", (req, res) => {
   const { url } = req.body;
   const randomString = generateRandomString();
   const shortUrl = `${randomString}`;
+  const urlModel = new Url({
+    originUrl: url,
+    shortUrl: shortUrl,
+  });
+  urlModel
+    .save()
+    .then((savedUrl) => {
+      res.json(savedUrl);
+    })
+    .catch((error) => {
+      console.log("error", error);
+      res.status(404).json({ error: error });
+    });
+  // urls = urls.concat({ originUrl: url, shortUrl: shortUrl });
 
-  urls = urls.concat({ originUrl: url, shortUrl: shortUrl });
-
-  res.json({ shortUrl });
+  // res.json({ shortUrl });
 });
 
-app.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const url = urls.find((url) => url.shortUrl === id);
-  if (url) {
-    res.redirect(url.originUrl);
-  } else {
-    res.status(404).json({ error: "URL not found" });
+app.get("/:shortUrl", (req, res) => {
+  const { shortUrl } = req.params;
+  if (shortUrl) {
+    console.log("shortUrl", shortUrl);
+    Url.findOne({ shortUrl }).then((url) => {
+      if (url) {
+        res.redirect(url.originUrl);
+      } else {
+        res.status(404).json({ error: "URL not found" });
+      }
+    });
   }
 });
 
 app.get("/", (req, res) => {
-  return res.json({ urls });
+  Url.find({}).then((result) => {
+    return res.json({ result });
+  });
 });
 
 app.listen(PORT, () => {
