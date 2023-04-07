1b687a6e882bfc1c262d3675fc8a1be36cf0baab
Jack Tsai <jtsai@nexify.com.hk>
2023-04-07 16:31:47 +0800

    Fix router error

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const UrlRouter = require("./controller/URLController");
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/url/", UrlRouter);
+app.use("/api/url", UrlRouter);
 module.exports = app;
