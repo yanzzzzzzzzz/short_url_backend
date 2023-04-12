db53814834589f5c6612315c4d3280708cea3528
Jack Tsai <jtsai@nexify.com.hk>
2023-04-12 18:03:34 +0800

    update helper

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -12,6 +12,7 @@ const initialUrls = [
 ];
 const vaildUrl = "https://www.google.com.tw/";
 const invaildUrl = "asd.tw";
+const updateShortUrl = "ABCDEFG";
 const urlsInDb = async () => {
   const urls = await Url.find({});
   return urls.map((url) => url.toJSON());
@@ -21,10 +22,30 @@ const usersInDb = async () => {
   const users = await User.find({});
   return users.map((user) => user.toJSON());
 };
+
+const initialUsers = [
+  {
+    username: "mluukkai",
+    name: "Matti Luukkainen",
+    password: "salainen",
+  },
+  {
+    username: "root",
+    name: "root123",
+    password: "1234",
+  },
+  {
+    username: "ggallin",
+    name: "ggallin",
+    password: "ggallin1234",
+  },
+];
 module.exports = {
   initialUrls,
   vaildUrl,
   invaildUrl,
+  updateShortUrl,
+  initialUsers,
   urlsInDb,
   usersInDb,
 };
