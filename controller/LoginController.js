eeb1b1a379e54c994f7aef4cb80e64c52307382e
Jack Tsai <jtsai@nexify.com.hk>
2023-04-20 10:50:54 +0800

    Add secret string in env

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const loginRouter = require("express").Router();
 const User = require("../models/user");
-
+const config = require("../utils/config");
 loginRouter.post("/", async (req, res) => {
   const { username, password } = req.body;
 
@@ -16,7 +16,7 @@ loginRouter.post("/", async (req, res) => {
 
   const userForToken = { username: user.username, id: user._id };
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
+  const token = jwt.sign(userForToken, config.SECRET, { expiresIn: 3600 });
 
   res.status(200).send({ token, username: user.username, name: user.name });
 });
--- utils/config.js
+++ utils/config.js
@@ -5,7 +5,9 @@ const MONGODB_URI =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
+const SECRET = process.env.SECRET;
 module.exports = {
   PORT,
   MONGODB_URI,
+  SECRET,
 };
