fa5477b47327dbb84456c2548778b918c8caacdf
Jack Tsai <jtsai@nexify.com.hk>
2024-03-19 15:00:16 +0800

    Fix async

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -10,7 +10,7 @@ loginRouter.post('/', async (req, res) => {
   if (user === null) {
     return res.status(401).json({ error: 'invalid username or password' });
   }
-  const passwordCorrect = bcrypt.compare(password, user.passwordHash);
+  const passwordCorrect = await bcrypt.compare(password, user.passwordHash);
 
   if (!passwordCorrect) {
     return res.status(401).json({ error: 'invalid username or password' });
