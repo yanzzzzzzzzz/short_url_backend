4ef7e23b459d86b02508362172c373aaf3adea50
yanzzzzzzzzz <king840110@gmail.com>
2024-05-30 17:26:53 +0800

    Add default config

--- utils/config.js
+++ utils/config.js
@@ -5,8 +5,8 @@ const MONGODB_URI =
   process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 const SECRET = process.env.SECRET;
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
-const REDIS_HOST = process.env.REDIS_HOST;
-const REDIS_PORT = process.env.REDIS_PORT;
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const GoogleClientId = process.env.GOOGLE_CLIENT_ID;
 const GoogleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
 const GoogleOauthRedirectUrl = process.env.GOOGLE_OAUTH_REDIRECT_URL;
