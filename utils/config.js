534ecff9ce7b87106e59f2e6c73dba47802b5958
yanzzzzzzzzz <king840110@gmail.com>
2024-06-03 15:02:03 +0800

    Update load .env file config

--- utils/config.js
+++ utils/config.js
@@ -1,25 +1,46 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT || 4001;
-const MONGODB_URI =
-  process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
-const SECRET = process.env.SECRET;
-const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
-const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
-const GoogleClientId = process.env.GOOGLE_CLIENT_ID;
-const GoogleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const GoogleOauthRedirectUrl = process.env.GOOGLE_OAUTH_REDIRECT_URL;
-const FrontEndUrl = process.env.FRONTEND_URL;
+const {
+  MONGODB_URI,
+  MONGODB_NAME,
+  NODE_ENV,
+  PORT: ENV_PORT,
+  SECRET,
+  REDIS_PASSWORD,
+  REDIS_HOST: ENV_REDIS_HOST,
+  REDIS_PORT: ENV_REDIS_PORT,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_OAUTH_REDIRECT_URL,
+  FRONTEND_URL
+} = process.env;
+
+const defaultDbName = 'URLShortenerDB';
+const defaultTestDbName = 'URLShortenerDBTest';
+
+const dockerConnectString = MONGODB_URI || 'mongodb://mongodb/';
+const localConnectString = MONGODB_URI || 'mongodb://localhost:27017/';
+
+const isDocker = NODE_ENV === 'docker';
+const isTest = NODE_ENV === 'test';
+
+const connectString = isDocker ? dockerConnectString : localConnectString;
+const dbName = isTest ? MONGODB_NAME || defaultTestDbName : MONGODB_NAME || defaultDbName;
+
+const MONGODB_URI_FINAL = `${connectString}${dbName}`;
+const PORT = ENV_PORT || 4001;
+const REDIS_HOST = ENV_REDIS_HOST || 'localhost';
+const REDIS_PORT = ENV_REDIS_PORT || 6379;
+
 module.exports = {
   PORT,
-  MONGODB_URI,
+  MONGODB_URI: MONGODB_URI_FINAL,
   SECRET,
   REDIS_PASSWORD,
   REDIS_HOST,
   REDIS_PORT,
-  GoogleClientId,
-  GoogleClientSecret,
-  GoogleOauthRedirectUrl,
-  FrontEndUrl
+  GoogleClientId: GOOGLE_CLIENT_ID,
+  GoogleClientSecret: GOOGLE_CLIENT_SECRET,
+  GoogleOauthRedirectUrl: GOOGLE_OAUTH_REDIRECT_URL,
+  FrontEndUrl: FRONTEND_URL
 };
