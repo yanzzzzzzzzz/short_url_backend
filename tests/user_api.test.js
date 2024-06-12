ffca3b6122e966f291768ab7b3a9d60ea8d65649
yanzzzzzzzzz <king840110@gmail.com>
2024-06-12 16:36:38 +0800

    Test for update user

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -111,6 +111,72 @@ describe('POST /api/users', () => {
   }, 50000);
 });
 
+describe('PATCH', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+    for (let index = 0; index < helper.initialUsers.length; index++) {
+      const initialUser = helper.initialUsers[index];
+      const passwordHash = await bcrypt.hash(initialUser.password, 10);
+      const user = new User({
+        username: initialUser.username,
+        email: initialUser.email,
+        passwordHash
+      });
+      await user.save();
+    }
+  });
+
+  afterEach(async () => {
+    await User.deleteMany({});
+  });
+
+  test('update user email', async () => {
+    const usersAtStart = await helper.usersInDb();
+    const testUser = helper.initialUsers[0];
+    const newEmail = 'newMail@com.tw';
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ email: testUser.email, password: testUser.password })
+      .expect(200);
+    const cookie = loginResponse.headers['set-cookie'];
+    await api.patch('/api/users').set('Cookie', cookie).send({ email: newEmail }).expect(204);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd[0].email).toEqual(newEmail);
+  });
+
+  test('update username', async () => {
+    const testUser = helper.initialUsers[0];
+    const newName = 'newName';
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ email: testUser.email, password: testUser.password })
+      .expect(200);
+    const cookie = loginResponse.headers['set-cookie'];
+    await api.patch('/api/users').set('Cookie', cookie).send({ username: newName }).expect(204);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd[0].username).toEqual(newName);
+  });
+
+  test('update username and email', async () => {
+    const testUser = helper.initialUsers[0];
+    const newInfo = { email: 'newEmail', username: 'newUserName' };
+
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ email: testUser.email, password: testUser.password })
+      .expect(200);
+    const cookie = loginResponse.headers['set-cookie'];
+    await api.patch('/api/users').set('Cookie', cookie).send(newInfo).expect(204);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd[0].username).toEqual(newInfo.username);
+    expect(usersAtEnd[0].email).toEqual(newInfo.email);
+  });
+});
 afterAll(async () => {
   await redisClient.disconnect();
   await mongoose.connection.close();
