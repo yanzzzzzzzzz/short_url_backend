81b015e5617c9ddb5cb65506f669d3b88323c717
yanzzzzzzzzz <king840110@gmail.com>
2024-06-13 18:00:12 +0800

    Add token authentication API

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const auth = require('../utils/auth');
+const config = require('../utils/config');
+const jwt = require('jsonwebtoken');
+const expiredSecond = 3600;
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
@@ -17,4 +20,18 @@ exports.login = async (req, res) => {
 
   auth.setAuthCookies(user, res);
   return res.status(200).end();
+};
+
+exports.authentication = async (req, res) => {
+  const token = req.cookies.customToken;
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  jwt.verify(token, config.SECRET, (err, decoded) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    res.status(200).end();
+  });
 };
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -3,5 +3,6 @@ const loginRouter = express.Router();
 const loginController = require('../controller/LoginController');
 
 loginRouter.post('/', loginController.login);
+loginRouter.get('/authentication', loginController.authentication);
 
 module.exports = loginRouter;
