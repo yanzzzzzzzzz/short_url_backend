e6598fd5d99943fdd4e6ed52fc7088056d3419c6
yanzzzzzzzzz <king840110@gmail.com>
2024-06-13 18:00:37 +0800

    Test delete user

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -177,6 +177,42 @@ describe('PATCH', () => {
     expect(usersAtEnd[0].email).toEqual(newInfo.email);
   });
 });
+
+describe('DELETE', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+    for (let index = 0; index < helper.initialUsers.length; index++) {
+      const initialUser = helper.initialUsers[index];
+      const passwordHash = await bcrypt.hash(initialUser.password, 10);
+      const user = new User({
+        username: initialUser.username,
+        email: initialUser.email,
+        passwordHash
+      });
+      await user.save();
+    }
+  });
+
+  afterEach(async () => {
+    await User.deleteMany({});
+  });
+  test('normal delete', async () => {
+    const testUser = helper.initialUsers[0];
+    const loginResponse = await api
+      .post('/api/login')
+      .send({ email: testUser.email, password: testUser.password })
+      .expect(200);
+
+    const cookie = loginResponse.headers['set-cookie'];
+    await api.delete('/api/users').set('Cookie', cookie).expect(204);
+    const deletedUser = await User.findOne({ email: testUser.email });
+    expect(deletedUser).toBeNull();
+  });
+  test('not login and delete will error', async () => {
+    await api.delete('/api/users').expect(401);
+  });
+});
+
 afterAll(async () => {
   await redisClient.disconnect();
   await mongoose.connection.close();
